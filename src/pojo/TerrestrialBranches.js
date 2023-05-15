3ca01c55dbadea934e89c6adc15650e21e3194c8
lax <laxlax@tutanota.com>
2023-05-15 20:09:52 +0800

    update

--- src/pojo/TerrestrialBranches.js
+++ src/pojo/TerrestrialBranches.js
@@ -4,7 +4,7 @@
  * @Author: lax
  * @Date: 2023-05-05 23:10:26
  * @LastEditors: lax
- * @LastEditTime: 2023-05-05 23:37:39
+ * @LastEditTime: 2023-05-06 21:36:33
  */
 const TERRESTRIAL_BRANCHES = [
 	"子",
@@ -20,18 +20,38 @@ const TERRESTRIAL_BRANCHES = [
 	"戌",
 	"亥",
 ];
+const { Phases } = require("tao_taichi.js");
 const inspect = Symbol.for("nodejs.util.inspect.custom");
-class TerrestrialBranches {
+class TerrestrialBranches extends Phases {
 	constructor(index) {
 		if (index instanceof TerrestrialBranches) return index;
 		// 文字取序号，数字取周期后序列
-		this.index =
+		const i =
 			~~(index + 1) === 0 ? TERRESTRIAL_BRANCHES.indexOf(index) : ~~index % 12;
+		if (i < 0) throw new Error("arg can`t be use");
+		// 水土木木土火火土金金土水
+		super(i % 3 === 1 ? 4 : ((~~((i + 1) / 3) % 4) + 1) % 4, (i + 1) % 2);
+		this.index = i;
 	}
 
 	getValue(is = false) {
 		return is ? TERRESTRIAL_BRANCHES[this.index] : this.index;
 	}
+
+	// 合
+	// 01/211/310/49/58/67/
+	combination() {
+		return new TerrestrialBranches((13 - this.index) % 12);
+	}
+
+	// 冲
+	conflict() {}
+
+	// 刑
+	punishment() {}
+
+	// 害
+	harm() {}
 
 	[inspect]() {
 		return this.getValue();
--- test/tb.spec.js
+++ test/tb.spec.js
@@ -4,7 +4,7 @@
  * @Author: lax
  * @Date: 2023-05-06 20:24:29
  * @LastEditors: lax
- * @LastEditTime: 2023-05-06 21:10:16
+ * @LastEditTime: 2023-05-15 19:59:38
  */
 const TerrestrialBranches = require("@/pojo/TerrestrialBranches");
 describe("地支", () => {
@@ -31,4 +31,31 @@ describe("地支", () => {
 			expect(tb.getLogos(true)).toBe(logos);
 		});
 	}
+});
+
+describe("地支克应-合", () => {
+	it(`子丑合`, () => {
+		expect(new TerrestrialBranches(0).combination().getValue(true)).toBe("丑");
+		expect(new TerrestrialBranches(1).combination().getValue(true)).toBe("子");
+	});
+	it(`寅亥合`, () => {
+		expect(new TerrestrialBranches(2).combination().getValue(true)).toBe("亥");
+		expect(new TerrestrialBranches(11).combination().getValue(true)).toBe("寅");
+	});
+	it(`辰酉合`, () => {
+		expect(new TerrestrialBranches(4).combination().getValue(true)).toBe("酉");
+		expect(new TerrestrialBranches(9).combination().getValue(true)).toBe("辰");
+	});
+	it(`午未合`, () => {
+		expect(new TerrestrialBranches(6).combination().getValue(true)).toBe("未");
+		expect(new TerrestrialBranches(7).combination().getValue(true)).toBe("午");
+	});
+	it(`申巳合`, () => {
+		expect(new TerrestrialBranches(8).combination().getValue(true)).toBe("巳");
+		expect(new TerrestrialBranches(5).combination().getValue(true)).toBe("申");
+	});
+	it(`戌卯合`, () => {
+		expect(new TerrestrialBranches(10).combination().getValue(true)).toBe("卯");
+		expect(new TerrestrialBranches(3).combination().getValue(true)).toBe("戌");
+	});
 });
