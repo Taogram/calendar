c6d856ebf0f7215894f41a4f9cda8da84e82693a
lax <laxlax@tutanota.com>
2024-07-26 08:22:52 +0800

    update

--- test/algorithm/algo.spec.js
+++ test/algorithm/algo.spec.js
@@ -4,7 +4,7 @@
  * @Author: lax
  * @Date: 2023-04-12 20:16:08
  * @LastEditors: lax
- * @LastEditTime: 2024-07-20 19:44:32
+ * @LastEditTime: 2024-07-25 21:47:38
  */
 const moment = require("moment");
 const algo = require("@/algorithm/SolarCalendar");
@@ -13,16 +13,17 @@ const _2022 = require("./2022");
 
 describe("算法测试：年干支", () => {
 	const years = [
-		["-002696-10-14T14:00:00.000Z", "甲子"],
-		["2021-09-05", "辛丑"],
-		["2024-02-03", "癸卯"],
-		["2024-02-04", "甲辰"],
-		["2024-02-05", "甲辰"],
+		["-002696-10-14T14:00:00.000Z", "甲子", false],
+		["2021-09-05", "辛丑", false],
+		["2024-02-03", "癸卯", false],
+		["2024-02-04", "甲辰", false],
+		["2024-02-04", "癸卯", true],
+		["2024-02-05", "甲辰", false],
 	];
-	years.map(([time, cstb]) => {
+	years.map(([time, cstb, accuracy]) => {
 		const t = moment(time).format("YYYY-MM-DD:HH-mm:ss");
 		it(`date: ${t} = ${cstb}}`, () => {
-			expect(SC_ARR[algo(time)[0][0]]).toBe(cstb);
+			expect(SC_ARR[algo(time, undefined, { accuracy })[0][0]]).toBe(cstb);
 		});
 	});
 });
@@ -32,18 +33,18 @@ describe("算法测试：月干支", () => {
 		const index = (((i * 2) % 10) + 2) % 10;
 		const time = new Date(`${1984 + i}-03-01 00:00:00`);
 		const [year, month] = algo(time)[0];
-		it(`年干：${CS_ARR[i]}=>月干：${CS_ARR[index]}`, () => {
+		it(`年干：${CS_ARR[i]}(${SC_ARR[year]})=>月干：${CS_ARR[index]}(${SC_ARR[month]})`, () => {
 			expect(SC_ARR[year][0]).toBe(CS_ARR[i]);
 			expect(SC_ARR[month][0]).toBe(CS_ARR[index]);
 		});
 	}
 
-	_2022.map(([t, index]) => {
+	_2022.map(([t, index, accuracy]) => {
 		const time = new Date(t);
-		const month = algo(time)[0][1];
+		const month = algo(time, undefined, { accuracy })[0][1];
 		it(`时间：${moment(time).format("YYYY-MM-DD HH:mm")}=>月支：${
 			TB_ARR[index]
-		}`, () => {
+		}(${SC_ARR[month]}) ${accuracy}`, () => {
 			expect(SC_ARR[month][1]).toBe(TB_ARR[index]);
 		});
 	});
@@ -52,7 +53,7 @@ describe("算法测试：月干支", () => {
 describe("算法测试: 日干支", () => {
 	const AD = new Date("2023-02-06 14:00:00");
 	it("日干支计算(AD): 乙未/31", () => {
-		expect(algo(AD)[0][2]).toBe(31);
+		expect(SC_ARR[algo(AD)[0][2]]).toBe("乙未");
 	});
 
 	it("日干支计算(AD): 乙未/31 -origin->2023-05-06", () => {
